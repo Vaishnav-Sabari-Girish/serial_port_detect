a9be92a3cb1305ce7cb50fe1bf0ca57ed76c7a8a
Vaishnav-Sabari-Girish <forgamesonly684@gmail.com>
2025-07-20 20:09:56 +0530

    Added working program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use serialport;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ports = serialport::available_ports()?;
+    
+    for port in ports {
+        // Only show USB ports
+        if let serialport::SerialPortType::UsbPort(ref info) = port.port_type {
+            println!("Port: {}", port.port_name);
+            println!("Type: {:?}", port.port_type);
+            println!("  USB VID: {:04x}, PID: {:04x}", info.vid, info.pid);
+            if let Some(manufacturer) = &info.manufacturer {
+                println!("  Manufacturer: {}", manufacturer);
+            }
+            if let Some(product) = &info.product {
+                println!("  Product: {}", product);
+            }
+            println!();
+        }
+    }
+    Ok(())
 }
